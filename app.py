5c4c5a643c72c08335a2c78eb7fbf7e96998b180
TimothyRimu <timothymrimu@gmail.com>
2024-09-06 20:00:13 +0300

    commit diabetes app to githuh

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# create a title for your app
+st.title ('Machine Learning App')
+
+# adding title for the app
+st.header('Header')
+
+# adding subheader to streamlit
+st.subheader('Subheader')
+
+# adding text to streamlit
+st.text('Normal text')
+st.markdown('Markdown text') 
+
+# adding data to streamlit
+data_dict = {'Course': ['Python', 'Machine Learning', 'Deep Learning', 'Data Science'],
+             'Students': [100, 200, 150, 300],
+             'Duration': [2, 3, 4, 6]}
+
+df = pd.DataFrame(data_dict)
+st.dataframe(df)
+
+
+ 
